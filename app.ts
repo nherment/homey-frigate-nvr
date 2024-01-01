1099d6e7c86cce744931802a93f5b4b307b95965
Nicolas Herment <nherment@arkhaios.net>
2024-01-01 19:02:32 +0100

    Add Homey Log

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 'use strict';
 
 import Homey from 'homey';
+const { Log } = require('homey-log');
 
 class MyApp extends Homey.App {
-
+  homeyLog:any;
   async onInit() {
+    this.homeyLog = new Log({ homey: this.homey });
   }
 
 }
